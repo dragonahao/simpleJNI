559baf3a700dee98735c9c06fe0898c93e560fe8
zylc369 <buwai.bw@qq.com>
2015-02-17 15:18:02 +0800

    删除古河的与注入有关的代码

--- include/process-thread.h
+++ include/process-thread.h
@@ -65,26 +65,6 @@ void* GetRemoteAddress(pid_t target_pid, const char* modulePath, const char* mod
 
 //////////////////////////////////////////////////////////////////////////
 
-int ptrace_readdata( pid_t pid,  uint8_t *src, uint8_t *buf, size_t size );
-
-int ptrace_writedata( pid_t pid, uint8_t *dest, uint8_t *data, size_t size );
-
-int ptrace_writestring( pid_t pid, uint8_t *dest, char *str  );
-
-int ptrace_call( pid_t pid, uint32_t addr, long *params, uint32_t num_params, struct pt_regs* regs );
-
-int ptrace_getregs( pid_t pid, struct pt_regs* regs );
-
-int ptrace_setregs( pid_t pid, struct pt_regs* regs );
-
-int ptrace_continue( pid_t pid );
-
-int ptrace_attach( pid_t pid );
-
-int ptrace_detach( pid_t pid );
-
-//////////////////////////////////////////////////////////////////////////
-
 /**
  * 远程内存。
  */
